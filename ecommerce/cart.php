c4caa641c5e4b3911430328d0ab9547e7cd14735
Renz Martin C. Cerico <cerico.renzmartin@gmail.com>
2021-04-03 21:33:26 +0800

    template

---
+++ ecommerce/cart.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>The Good Mob Movement | Cart</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!-- Lightbox-->
+    <link rel="stylesheet" href="vendor/lightbox2/css/lightbox.min.css">
+    <!-- Range slider-->
+    <link rel="stylesheet" href="vendor/nouislider/nouislider.min.css">
+    <!-- Bootstrap select-->
+    <link rel="stylesheet" href="vendor/bootstrap-select/css/bootstrap-select.min.css">
+    <!-- Owl Carousel-->
+    <link rel="stylesheet" href="vendor/owl.carousel2/assets/owl.carousel.min.css">
+    <link rel="stylesheet" href="vendor/owl.carousel2/assets/owl.theme.default.css">
+    <!-- Google fonts-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Libre+Franklin:wght@300;400;700&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Martel+Sans:wght@300;400;800&amp;display=swap">
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="css/style.blue.min.css" id="theme-stylesheet">
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="css/custom.css">
+    <!-- Favicon-->
+    <link rel="icon" href="assets/img/thegoodmob-emblem.png">
+    <script src="vendor/jquery/jquery.min.js"></script>
+  </head>
+  <body>
+    <div class="page-holder">
+      <!-- begin:: navbar-->
+      <?php
+        include 'shared/navigation.html';
+      ?>
+      <!-- end:: navbar -->
+      <div class="container">
+        <section class="py-5">
+          <h2 class="h5 text-uppercase mb-4">Shopping cart</h2>
+          <div class="row">
+            <div class="col-lg-8 mb-4 mb-lg-0">
+              <!-- CART TABLE-->
+              <div class="table-responsive mb-4">
+                <table class="table">
+                  <thead class="bg-light">
+                    <tr>
+                      <th class="border-0" scope="col"> <strong class="text-small text-uppercase">Product</strong></th>
+                      <th class="border-0" scope="col"> <strong class="text-small text-uppercase">Price</strong></th>
+                      <th class="border-0" scope="col"> <strong class="text-small text-uppercase">Quantity</strong></th>
+                      <th class="border-0" scope="col"> <strong class="text-small text-uppercase">Total</strong></th>
+                      <th class="border-0" scope="col"> </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th class="pl-0 border-0" scope="row">
+                        <div class="media align-items-center"><a class="reset-anchor d-block animsition-link" href="detail.php"><img src="img/product-detail-3.jpg" alt="..." width="70"/></a>
+                          <div class="media-body ml-3"><strong class="h6"><a class="reset-anchor animsition-link" href="detail.php">Red digital smartwatch</a></strong></div>
+                        </div>
+                      </th>
+                      <td class="align-middle border-0">
+                        <p class="mb-0 small">₱250</p>
+                      </td>
+                      <td class="align-middle border-0">
+                        <div class="border d-flex align-items-center justify-content-between px-3"><span class="small text-uppercase text-gray headings-font-family">Quantity</span>
+                          <div class="quantity">
+                            <button class="dec-btn p-0"><i class="fas fa-caret-left"></i></button>
+                            <input class="form-control form-control-sm border-0 shadow-0 p-0" type="text" value="1"/>
+                            <button class="inc-btn p-0"><i class="fas fa-caret-right"></i></button>
+                          </div>
+                        </div>
+                      </td>
+                      <td class="align-middle border-0">
+                        <p class="mb-0 small">₱250</p>
+                      </td>
+                      <td class="align-middle border-0"><a class="reset-anchor" href="#"><i class="fas fa-trash-alt small text-muted"></i></a></td>
+                    </tr>
+                    <tr>
+                      <th class="pl-0 border-light" scope="row">
+                        <div class="media align-items-center"><a class="reset-anchor d-block animsition-link" href="detail.php"><img src="img/product-detail-2.jpg" alt="..." width="70"/></a>
+                          <div class="media-body ml-3"><strong class="h6"><a class="reset-anchor animsition-link" href="detail.php">Apple watch</a></strong></div>
+                        </div>
+                      </th>
+                      <td class="align-middle border-light">
+                        <p class="mb-0 small">₱250</p>
+                      </td>
+                      <td class="align-middle border-light">
+                        <div class="border d-flex align-items-center justify-content-between px-3"><span class="small text-uppercase text-gray headings-font-family">Quantity</span>
+                          <div class="quantity">
+                            <button class="dec-btn p-0"><i class="fas fa-caret-left"></i></button>
+                            <input class="form-control form-control-sm border-0 shadow-0 p-0" type="text" value="1"/>
+                            <button class="inc-btn p-0"><i class="fas fa-caret-right"></i></button>
+                          </div>
+                        </div>
+                      </td>
+                      <td class="align-middle border-light">
+                        <p class="mb-0 small">₱250</p>
+                      </td>
+                      <td class="align-middle border-light"><a class="reset-anchor" href="#"><i class="fas fa-trash-alt small text-muted"></i></a></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!-- CART NAV-->
+              <div class="bg-light px-4 py-3">
+                <div class="row align-items-center text-center">
+                  <div class="col-md-6 mb-3 mb-md-0 text-md-left"><a class="btn btn-link p-0 text-dark btn-sm" href="products.php"><i class="fas fa-long-arrow-alt-left mr-2"> </i>Continue shopping</a></div>
+                  <div class="col-md-6 text-md-right"><a class="btn btn-outline-dark btn-sm" href="checkout.php">Proceed to checkout<i class="fas fa-long-arrow-alt-right ml-2"></i></a></div>
+                </div>
+              </div>
+            </div>
+            <!-- ORDER TOTAL-->
+            <div class="col-lg-4">
+              <div class="card border-0 rounded-0 p-lg-4 bg-light">
+                <div class="card-body">
+                  <h5 class="text-uppercase mb-4">Cart total</h5>
+                  <ul class="list-unstyled mb-0">
+                    <li class="d-flex align-items-center justify-content-between"><strong class="text-uppercase small font-weight-bold">Subtotal</strong><span class="text-muted small">₱500</span></li>
+                    <li class="border-bottom my-2"></li>
+                    <li class="d-flex align-items-center justify-content-between mb-4"><strong class="text-uppercase small font-weight-bold">Total</strong><span>₱500</span></li>
+                    <li>
+                      <form action="#">
+                        <div class="form-group mb-0">
+                          <input class="form-control" type="text" placeholder="Enter your coupon">
+                          <button class="btn btn-dark btn-sm btn-block" type="submit"> <i class="fas fa-gift mr-2"></i>Apply coupon</button>
+                        </div>
+                      </form>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <!-- begin:: Footer -->
+      <?php
+        include 'shared/footer.html';
+      ?>
+      <!-- end:: Footer -->
+      <!-- JavaScript files-->
+      <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+      <script src="vendor/lightbox2/js/lightbox.min.js"></script>
+      <script src="vendor/nouislider/nouislider.min.js"></script>
+      <script src="vendor/bootstrap-select/js/bootstrap-select.min.js"></script>
+      <script src="vendor/owl.carousel2/owl.carousel.min.js"></script>
+      <script src="vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.min.js"></script>
+      <script src="js/front.js"></script>
+      <!-- FontAwesome CSS - loading as last, so it doesn't block rendering-->
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    </div>
+  </body>
+</html>
