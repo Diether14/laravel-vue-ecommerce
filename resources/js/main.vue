35a5b69cf03c29d0ca87da860dc137492180034c
= <=>
2021-04-25 22:06:06 +0800

    vue integration

---
+++ resources/js/main.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <kt-card title="hello"></kt-card>
+    </div>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log('Component mounted.')
+    }
+}
+</script>
+
+<style>
+
+</style>
