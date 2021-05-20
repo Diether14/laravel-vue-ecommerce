2e632b30f684b9a87d4808e24ce753e88045953e
Renz Martin C. Cerico <cerico.renzmartin@gmail.com>
2021-05-20 23:48:04 +0800

    added script for add to card & wishlist

--- resources/views/shared/products/index.blade.php
+++ resources/views/shared/products/index.blade.php
@@ -3,8 +3,8 @@
     <div class="badge text-white badge-"></div><a class="d-block" href="detail.php"><img class="img-fluid w-100" src="img/product-1.jpg" alt="..."></a>
     <div class="product-overlay">
       <ul class="mb-0 list-inline">
-        <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-outline-dark" href="#"><i class="far fa-heart"></i></a></li>
-        <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-dark" href="cart.php">Add to cart</a></li>
+        <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-outline-dark add-to-wishlist" href="#"><i class="far fa-heart"></i></a></li>
+        <li class="list-inline-item m-0 p-0"><a class="btn btn-sm btn-dark add-to-cart" href="#">Add to cart</a></li>
         <li class="list-inline-item mr-0"><a class="btn btn-sm btn-outline-dark" href="#productView" data-toggle="modal"><i class="fas fa-expand"></i></a></li>
       </ul>
     </div>
@@ -12,3 +12,19 @@
   <h6> <a class="reset-anchor" href="detail.php">Kui Ye Chen’s AirPods</a></h6>
   <p class="small text-muted">₱250</p>
 </div>
+
+<script>
+  const Product = {
+    addToCart: () => {
+      alert('added to cart');
+    },
+    addToWishList: () => {
+      alert('added to wishlist');
+    }
+  };
+
+  $(document).ready(() => {
+    $(document).on('click','.add-to-cart', () => Product.addToCart());
+    $(document).on('click','.add-to-wishlist', () => Product.addToWishList());
+  });
+</script>
