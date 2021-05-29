0581154e1aa3ba6aa184caa5415842e81f10be6a
Renz Martin C. Cerico <cerico.renzmartin@gmail.com>
2021-05-30 04:53:58 +0800

    fix landing page

--- resources/views/store_owner/pages/products/update.blade.php
+++ resources/views/store_owner/pages/products/update.blade.php
@@ -7,7 +7,7 @@
       <div class="card-body">
         <div class="form-group">
           <label for="productName">Name</label>
-          <input type="text" name="name" class="form-control" id="productName" value="Test Product" placeholder="Enter name">
+          <input type="text" name="name" class="form-control" id="productName" value="" placeholder="Enter name">
         </div>
         <div class="form-group">
           <label>Categories</label>
@@ -24,11 +24,11 @@
         </div>
         <div class="form-group">
             <label for="productPrice">Price</label>
-            <input type="number" name="price" class="form-control" id="productPrice" value="100" placeholder="Enter price">
+            <input type="number" name="price" class="form-control" id="productPrice" value="" placeholder="Enter price">
         </div>
         <div class="form-group">
             <label for="productStocks">Stocks</label>
-            <input type="number" name="stocks" class="form-control" id="productStocks" value="100" placeholder="Enter stocks">
+            <input type="number" name="stocks" class="form-control" id="productStocks" value="" placeholder="Enter stocks">
         </div>
       </div>
       <!-- /.card-body -->
@@ -49,6 +49,22 @@
   <script>
     $(document).ready(() => {
       const Product = {
+        get: () => {
+          return new Promise((resolve, reject) => {
+            const url_string = window.location.href
+            const url = new URL(url_string);
+            const urlSplit = url.pathname.split('/');
+            const id = urlSplit[4];
+
+              $.ajax({
+                  url: '/owner/api/products/' + id,
+                  method: 'GET',
+                  success: (res) => {
+                      resolve(res);
+                  }
+              });
+          });
+        },
         update: (e) => {
             const url_string = window.location.href
             const url = new URL(url_string);
@@ -74,9 +90,18 @@
                     window.location.href = '/owner/products';
                 }
             });
+        },
+        show: async () => {
+          const product = JSON.parse(await Product.get());
+          console.log(product);
+          $('#productName').val(product.data[0].name);
+          $('#productDescription').val(product.data[0].description);
+          $('#productPrice').val(product.data[0].price);
+          $('#productStocks').val(product.data[0].stocks);
         }
       };
 
+      Product.show();
       $(document).on('submit', '#quickForm', (e) => Product.update(e));
     });
   </script>
